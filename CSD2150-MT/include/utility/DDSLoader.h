007f3f62e0d238a7304fad45bf22828fa119301b
owenwensong <w.huang@digipen.edu>
2022-03-20 22:23:47 +0800

    file prep for DDSLoader

---
+++ CSD2150-MT/include/utility/DDSLoader.h
@@ -0,0 +1,17 @@
+/*!*****************************************************************************
+ * @file    DDSLoader.h
+ * @author  Owen Huang Wensong  (w.huang@digipen.edu)
+ * @date    20 MAR 2022
+ * @brief   This file contains the declarations for a DDS loader
+ * 
+ * @par     reference: https://docs.microsoft.com/en-us/windows/win32/direct3ddds/dx-graphics-dds-pguide?redirectedfrom=MSDN
+ *
+ * @par Copyright (C) 2022 DigiPen Institute of Technology. All rights reserved.
+*******************************************************************************/
+
+#ifndef UTILITY_DDS_LOADER_HELPER_HEADER
+#define UTILITY_DDS_LOADER_HELPER_HEADER
+
+
+
+#endif//UTILITY_DDS_LOADER_HELPER_HEADER
